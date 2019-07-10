540dc7f16a9dd269ffa4a638c78ddf943ca2fa5f
Manju S V <manjusvijaykumar@gmail.com>
2019-07-10 17:03:01 +0530

    Stack Plots - matplotlib series

---
+++ StackPlots/demo.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+
+plt.style.use("fivethirtyeight")
+
+minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+player1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
+player2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
+player3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+labels = ['player1', 'player2', 'player3']
+colors = ['#6d904f', '#fc4f30', '#008fd5']
+
+plt.stackplot(minutes, player1, player1, player2, labels=labels, colors=colors)
+plt.legend(loc='upper right')
+plt.title("My Stack Plot")
+plt.tight_layout()
+plt.savefig('plot.png')
+plt.show()
